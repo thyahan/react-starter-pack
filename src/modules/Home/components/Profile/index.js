63cc31ba6162d982ab34491cfa898e623932a834
Yutthana Yahan <yutthana.y@gogoprint.com>
2018-05-05 15:24:35 +0700

    UPdate useless files

--- src/modules/Home/components/Profile/index.js
+++ src/modules/Home/components/Profile/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
-import A from '../Test';
 
 class Profile extends Component {
     render() {
         return (
             <div className='profile'>
-                {/* <div className='cover'></div> 
+                <div className='cover'></div> 
                 <div className='content'>
                     <div className='content-left'></div>
                     <div className='content-center'></div>
                     <div className='content-right'></div>
-                </div> */}
-                <A />
+                </div>
             </div>
         );
     }
